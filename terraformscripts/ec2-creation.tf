a1efc8f727ef51f8cdf280ddcd92c587e894bca8
mantu 117 <110002261+Mantesh17@users.noreply.github.com>
2022-12-20 19:12:50 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,13 @@
 provider "aws"{
-   region  = "ap-southeast-1" 
+   region  = "ap-south-1" 
 }
 
 resource "aws_instance" "AWSEC2Instance"{
-     count   = 1
-     ami = "ami-010d564ad44bd1912"
+     count   = 2
+     ami = "ami-0cca134ec43cf708f"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
+     key_name        = "mantesh"
      tags = {
         Name = "tomcatservers"
      }
